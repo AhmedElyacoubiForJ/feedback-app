4c72344df1824aca48869d4619983cce11a2c830
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-03-08 15:02:06 +0100

    events & prop Drilling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
 
   const deleteFeedback = (id) => {
-    console.log('App', id)
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter( (item) => item.id !== id))
+    }
+    // console.log('App', id)
+
   }
   
   return (
