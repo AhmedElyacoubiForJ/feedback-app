02287b77b11ad8862ceb063ac81de6a9158f7c17
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-03-08 16:31:36 +0100

    text form chaged wired w. the the state of FeedbackForm comp.

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,7 +5,7 @@ function FeedbackForm() {
   const [text, setText] = useState("");
 
   const handleTextChange = (e) => {
-    console.log(e.target.value)
+    setText(e.target.value)
   }
 
   return (
@@ -14,7 +14,7 @@ function FeedbackForm() {
         <h2>How would you rate servcie with us?</h2>
         {/*  @todo - rating delect component */}
         <div className="input-group">
-          <input onChange={handleTextChange} type="text" placeholder="Write a review"></input>
+          <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text}></input>
           <button type="submit">Send</button>
         </div>
       </form>
