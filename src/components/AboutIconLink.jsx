3a2e457b3c0a863889b1a5c4936927df9664cdde
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-03-10 16:08:05 +0100

    custom link as example

--- src/components/AboutIconLink.jsx
+++ src/components/AboutIconLink.jsx
@@ -1,10 +1,18 @@
 import { FaQuestion } from "react-icons/fa";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 function AboutIconLink() {
   return (
     <div className="about-link">
       <Link to="/about">
+      {/* to as a object & we can pass request parameters */}
+      {/* <Link
+        to={{
+          pathname: "/about",
+          search: '?sort=name',
+          hash: '#hello',
+        }}
+      > */}
         <FaQuestion size={30} />
       </Link>
     </div>
