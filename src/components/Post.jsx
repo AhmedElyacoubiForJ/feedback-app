eae04bbf4aa970e466d7a76cc665194b84edc718
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-03-11 13:48:18 +0100

    navigation using hook

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,5 @@
 //import { useParams } from "react-router-dom"
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 function Post() {
   /* const params = useParams()
     return (
@@ -8,13 +8,20 @@ function Post() {
     </div>
   ) */
   const status = 200;
+  const navigate = useNavigate();
+
   if (status === 404) {
     return <Navigate to="/notfound" />;
   }
 
+  const onClick = () => {
+    navigate('/about')
+  };
+
   return (
     <div>
-      <h1>Post:</h1>
+      <h1>Post</h1>
+      <button onClick={onClick}>Click</button>
     </div>
   );
 }
