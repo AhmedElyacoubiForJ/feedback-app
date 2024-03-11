6f2e360a35d6a8386efe9593f67be85d77a8ed19
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-03-11 16:28:20 +0100

    adding edit icon to feedbackItem

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
+import { FaTimes, FaEdit } from "react-icons/fa";
 
 import FeedbackContext from "../context/FeedbackContext";
-import { FaTimes } from "react-icons/fa";
 import { PropTypes } from "prop-types";
 import Card from "./shared/Card";
 
@@ -15,6 +15,9 @@ function FeedbackItem({ item }) {
       <div className="num-display">{item.rating}</div>
       <button onClick={() => deleteFeedback(item.id)} className="close">
         <FaTimes color="purple" />
+      </button>
+      <button className='edit'>
+        <FaEdit color='purple'/>
       </button>
       <div className="text-display">{item.text}</div>
     </Card>
