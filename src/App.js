de3cf56e94ff43e13fd91ccccb3cff6eebe00cca
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-03-11 13:01:10 +0100

    working w. NavLnik but is not part of this project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
@@ -8,6 +13,7 @@ import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from "./components/pages/AboutPage";
 import AboutIconLink from "./components/AboutIconLink";
+import Card from "./components/shared/Card";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -46,6 +52,16 @@ function App() {
             This is the about route
           </Route>
         </Routes>
+        
+        <Card>
+          <NavLink to="/" activeClassName="active">
+            Home
+          </NavLink>
+          <NavLink to="/about" activeClassName="active">
+            About
+          </NavLink>
+        </Card>
+
         <AboutIconLink />
       </div>
     </Router>
